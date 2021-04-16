4c71dfdc491afc44704118d97e84a2cc068b5e99
Esmir Rastoder <73173141+esmir-ras@users.noreply.github.com>
2021-04-16 12:55:55 +0200

    Commit

--- js/question-2.js
+++ js/question-2.js
@@ -7,6 +7,36 @@ const resultsContainer = document.querySelector(".results");
 
 async function getGames() {
 
+   try {
+      const response = await fetch(url);
+
+const apiresults = await response.json();
+
+const facts = apiresults.results
+
+resultsContainer.innerHTML = "";
+
+for (let i = 0; i < facts.length; i++) {
+   console.log(facts[i].name);
+   console.log(facts[i].rating)
+   console.log(facts[i].tags)
+   
+   if (i === 8) {
+      break;
+   }
+    
+   resultsContainer.innerHTML += `<div class="api-result"><b>Name</b>: ${facts[i].name}</div>`
+   resultsContainer.innerHTML += `<div class="api-result"><b>Rating</b> ${facts[i].rating}</div>`
+   resultsContainer.innerHTML += `<div class="api-result"><b>Tags</b> ${facts[i].tags.length}</div>`
+   
+   
+}
+   }
+   catch(error) { 
+      console.log("An error occured");
+      resultsContainer.innerHTML = displayError("Error Error!")
+   }
+
 const response = await fetch(url);
 
 const apiresults = await response.json();
@@ -34,6 +64,13 @@ for (let i = 0; i < facts.length; i++) {
 }
 
 getGames();
+
+// Error function
+
+function displayError(message) {
+return `<div class"error">${message}</div>`
+
+}
 
 
 
